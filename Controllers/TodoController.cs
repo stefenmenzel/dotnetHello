c26c7ac434d394861f3d0b9013103fdbf5128ff6
StefenMenzel <stefenmenzel@gmail.com>
2019-07-26 18:35:56 -0500

    initial commit

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if(_context.TodoItems.Count() == 0)
+            {
+                //Create a new TodoItem if collection is empty,
+                //Which means you can't delete all TodoItems.
+                _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+                _context.SaveChanges();
+            }
+        }
+
+        //Get: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        //Get: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var TodoItem = await _context.TodoItems.FindAsync(id);
+
+            if(TodoItem == null)
+            {
+                return NotFound();
+            }
+            return TodoItem;
+        }
+
+        [HttpGet("helloNurse")]
+        public async Task<ActionResult<string>> GetHello()
+        {
+            return "HELOOOOOOO NURSE";
+        }
+    }
+}
