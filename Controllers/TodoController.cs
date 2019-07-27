64110b8cce2fdea88c0daddca2b352079b7df70a
stefenmenzel <stefenmenzel@gmail.com>
2019-07-27 14:28:40 -0500

    Got some CRUD building up

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -51,5 +51,45 @@ namespace TodoApi.Controllers
         {
             return "HELOOOOOOO NURSE";
         }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem item)
+        {
+            _context.TodoItems.Add(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetTodoItem), new {id = item.Id}, item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem item)
+        {
+            if(id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        //Delete: api/Todo/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
